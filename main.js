e20869aabf6025b5b5387fc65e2218867de9d360
mallzganz <93106364+mallzganz@users.noreply.github.com>
2021-10-26 09:31:26 +0700

    Update main.js

--- main.js
+++ main.js
@@ -80,7 +80,7 @@ zeroyt7.on("group-participants-update", async (anu) => {
         let v = zeroyt7.contacts[num] || { notify: num.replace(/@.+/, "") }
         anu_user = v.vname || v.notify || num.split("@")[0]
         time_wel = moment.tz("Asia/Jakarta").format("HH:mm")
-        wel = `Halo @${anu_user} \nWelcome In ${mdata.subject} \nJangan Lupa Follow Sosial Media Ownerku\n• Youtube : https://youtube.com/ZeroYT7\n• Instagram : https://instagram.com/Zero_YT7\n• Tiktok : https://tiktok.com/@_zeroyt7\n• Github : https://github.com/Zero-YT7`
+        wel = `Halo @${anu_user} \nWelcome In ${mdata.subject} \nJangan Lupa PATUHI ATURAN GROUP`
         buff = await getBuffer(
           `http://hadi-api.herokuapp.com/api/card/welcome?nama=${anu_user}&descriminator=${groupMembers.length
           }&memcount=${memeg}&gcname=${encodeURI(
